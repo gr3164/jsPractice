1da208ff8764b4972335c2fae243ac573521bbf6
Grisha_Pinigin <pin.pinigin@mail.ru>
2023-08-26 10:54:27 +0500

    qwe

--- jsECMAScript/hw1/hw3.js
+++
@@ -1,9 +0,0 @@
-// 3) Напишем функцию, которая будет находить факториал числа с использованием рекурсии:
-// примеры вызова функции
-// console.log(factorial(5)); // выводит 120 (5 * 4 * 3 * 2 * 1)
-// console.log(factorial(0)); // выводит 1 (по определению факториала)
-
-const factorial = (num) => !num ? 1 : num * factorial(num - 1);
-
-console.log(factorial(5));
-console.log(factorial(0));
